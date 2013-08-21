38413b6a241b984b046d0187f00228dc17b1b38a
btao <btao@ebay.com>
2013-08-21 10:59:47 +0800

    Implement constructor 'init()'

--- TimeSeries.js
+++ TimeSeries.js
@@ -9,14 +9,16 @@
  * Version: 0.0
  */
 
-function TimeSeries(options) {
+function TimeSeries(obj, options) {
 
   /******************************\
           Private Methods
   \******************************/
   var self = this;  // Stores this
 
-  var list = [];    // Stores alist of tables
+  var T_TIME_SERIES_SET = 'TimeSeriesSet',
+      T_TIME_SERIES     = 'TimeSeries',
+      T_OTHER           = false;
 
   var isUndefined = function(v) {
     return (typeof v === 'undefined');
@@ -39,6 +41,14 @@ function TimeSeries(options) {
     return (typeof v === 'object');
   };
 
+  var getTimeSeriesType = function(v) {
+    return ('__type__' in v) ? v.__type__ : false;
+  };
+
+  var setTimeSeriesType = function(v, type) {
+    return v.__type__ = type;
+  };
+
   var extend = function(obj) { 
     for (var i = 0; i < arguments.length; i++)
       for (key in arguments[i])
@@ -48,9 +58,27 @@ function TimeSeries(options) {
     return obj;
   };
 
+  /* Copy plainObject/Array recursively CAUTION: no cycle!  */
+  var plainDeepCopy = function(obj) {
+    if (!isObject(obj))
+      return obj;
+    var t = {};
+    if (isArray(obj))
+      t = [];
+    foreach(obj, function(key, index, obj) {
+      t[key] = plainDeepCopy(obj[key]);
+    });
+    return t;
+  };
+
   var defValIfUndefined = function(x, defVal) {
     return (isUndefined(x)) ? defVal : x;
   };
+
+  var settings = extend({
+    dataType: 'time-dictionary',
+    inPlace: false,       // do all operations in place?
+  }, defValIfUndefined(options, {}));
 
   /*
    *  args should be a list of arguments
@@ -59,6 +87,18 @@ function TimeSeries(options) {
     if (!isFunction(f))
       return undefined;
     return f.apply(_this, args);
+  };
+
+  /* returns an Object's own keys */
+  var keys = function(obj) {
+    if (Object.keys)
+      return Object.keys();
+    // For IE
+    var keyList = [];
+    for (key in obj)
+      if (obj.hasOwnProperty(key))
+        keyList.push(key);
+    return keyList;
   };
 
   /*
@@ -74,14 +114,57 @@ function TimeSeries(options) {
           return;
   };
 
-  var settings = extend({
-    data: [
-      1234567890, {
-        'count': 100,
-        'field1': 'abc',
-      },
-    ],
-  }, options);
+  var createSeries = function(append, rawData) {
+    var series = {
+        fields: [],
+        data: [],
+      };
+    if (!isUndefined(rawData)) // Do the deepcopy
+      series = plainDeepCopy(rawData);
+    setTimeSeriesType(series, T_TIME_SERIES);
+    if (defValIfUndefined(append, true))
+      self[self.length++] = series;
+    return series;
+  };
+
+  var init = function() {
+    setTimeSeriesType(TimeSeries.prototype, T_TIME_SERIES_SET);
+    self.length = 0;
+
+    switch (getTimeSeriesType(obj)) {
+      case T_TIME_SERIES_SET:
+        // TODO: clone from 'obj'
+        for (var i = 0; i < obj.length; i++)
+          createSeries(true, obj[i]);
+        break;
+      default: {
+        // regard 'obj' as raw data
+        /* Format of data to feed in
+          (FIXME:Now we only support data in the same form as that inside): 
+              [
+                [TIMESTAMP, {
+                    attr1: 10,
+                    attr2: '',
+                    ...
+                  }
+                ],
+                ...
+              ]
+
+          Format of data inside TimeSeries:
+            {
+              fields: ['field1', 'f2', ...],
+              data:   [[TS, val1, ...], ...]
+            }
+        */
+        // Hand to T_TIME_SERIES for creating
+      }
+      case T_TIME_SERIES:
+        // construct the object from one time series 'obj'
+        createSeries(true, obj);
+        break;
+    };
+  };
 }
 
 /******************************\
